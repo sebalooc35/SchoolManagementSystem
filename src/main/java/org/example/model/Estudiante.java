56c39b4613c7332ec07425b3214518bf4f70c0c9
sebas <andres.ordonez@galileo.edu>
2024-04-14 17:57:11 -0600

    trasladar datos comunes de personas a clase Persona y hacer Estudiante heredar de persona.

--- src/main/java/org/example/model/Estudiante.java
+++ src/main/java/org/example/model/Estudiante.java
@@ -2,18 +2,13 @@ package org.example.model;
 
 import java.util.Date;
 
-public class Estudiante {
+public class Estudiante extends Persona{
     private Long id;
-    private String nombre;
-    private String apellido;
-    private Date fechaDeNacimiento;
     private EstadoEstudiante estado;
 
     public Estudiante(Long id, String nombre, String apellido, Date fechaDeNacimiento, EstadoEstudiante estado) {
+        super(nombre, apellido, fechaDeNacimiento);
         this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.fechaDeNacimiento = fechaDeNacimiento;
         this.estado = estado;
     }
 
@@ -25,29 +20,6 @@ public class Estudiante {
         this.id = id;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public Date getFechaDeNacimiento() {
-        return fechaDeNacimiento;
-    }
-
-    public void setFechaDeNacimiento(Date fechaDeNacimiento) {
-        this.fechaDeNacimiento = fechaDeNacimiento;
-    }
 
     public EstadoEstudiante getEstado() {
         return estado;
---
+++ src/main/java/org/example/model/Persona.java
@@ -0,0 +1,37 @@
+package org.example.model;
+
+import java.util.Date;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private Date fechaDeNacimiento;
+    public Persona(String nombre, String apellido, Date fechaDeNacimiento){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(Date fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+}
